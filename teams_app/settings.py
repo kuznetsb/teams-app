3a84f6eb56f47ba279b20fdd89b7ea37e1ea9967
kuznetsb <kuznetsbog@gmail.com>
2024-01-12 11:40:10 +0200

    postgres port added

--- teams_app/settings.py
+++ teams_app/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
         "NAME": env("POSTGRES_DB"),
         "USER": env("POSTGRES_USER"),
         "PASSWORD": env("POSTGRES_PASSWORD"),
