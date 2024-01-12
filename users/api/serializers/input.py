0091beb688aabedec9516988edc164d0389c3198
kuznetsb <kuznetsbog@gmail.com>
2024-01-12 22:53:08 +0200

    is staff possible to modify

--- users/api/serializers/input.py
+++ users/api/serializers/input.py
@@ -6,7 +6,6 @@ class UserInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "first_name", "last_name", "is_staff")
-        read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True}}
 
     def validate_password(self, value):
